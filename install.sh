3520a7374a7ff727abeb8033051a2d639a2ce145
EkeMinusYou <euonymuslke@gmail.com>
2022-08-04 01:28:22 +0900

    install.sh: fix ln path

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -10,18 +12,18 @@ if [ "$(uname)" == 'Darwin' ]; then
 fi
 
 # Clone dotfiles
-DOT_DIR="$HOME/src/dotfiles"
+DOT_DIR="$HOME/src/github.com/EkeMinusYou"
 git clone https://github.com/EkeMinusYou/dotfiles ${DOT_DIR}
-ln -sf $DOT_DIR/.gitconfig ~/.gitconfig
+ln -sfn $DOT_DIR/dotfiles/.gitconfig ~/.gitconfig
 
 # Install packages by brew
-brew bundle --file "$DOT_DIR/Brewfile"
+brew bundle --file "$DOT_DIR/dotfiles/Brewfile"
 
 # Setup fish
 $(brew --prefix)/opt/fzf/install
-ln -sf $DOT_DIR/.config/fish/config.fish ~/.config/fish/config.fish
-ln -sf $DOT_DIR/.config/fish/functions/select_ghq_cd.fish ~/.config/fish/functions/select_ghq_cd.fish
-ln -sf $DOT_DIR/.config/fish/functions/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
+ln -sfn $DOT_DIR/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -sfn $DOT_DIR/dotfiles/.config/fish/functions/select_ghq_cd.fish ~/.config/fish/functions/select_ghq_cd.fish
+ln -sfn $DOT_DIR/dotfiles/.config/fish/functions/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 
 # Setup neovim
-ln -sf $DOT_DIR/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sfn $DOT_DIR/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
