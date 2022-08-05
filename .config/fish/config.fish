ada9bbba8f79344cfdfda5be95da9d78e947b156
EkeMinusYou <euonymuslke@gmail.com>
2022-08-05 12:32:57 +0900

    fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,15 @@
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 # Python
-pyenv init - | source
+# set -x PYENV_ROOT $HOME/.pyenv
+# set -x PATH  $PYENV_ROOT/bin $PATH
+# pyenv init - | source
 
 # VSCode
 alias e='code -a .'
 
 # zoxide
 zoxide init fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '$HOME/google-cloud-sdk/path.fish.inc' ]; . '$HOME/google-cloud-sdk/path.fish.inc'; end
