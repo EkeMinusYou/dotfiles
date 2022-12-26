1b5dfedea9cc75c301672d0764f267ca1408fd7a
EkeMinusYou <euonymuslke@gmail.com>
2022-12-27 01:17:28 +0900

    add indent guides

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
 require('keymap')
 require('plugins')
-
-vim.api.nvim_set_var('fern#renderer', 'nerdfont')
-vim.api.nvim_set_var('fern#default_hidden', 1)
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,4 +6,11 @@ require('jetpack.packer').add {
   {'lambdalisue/fern-git-status.vim'},
   {'lambdalisue/nerdfont.vim'},
   {'lambdalisue/fern-renderer-nerdfont.vim'},
+  {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
+  {'nathanaelkane/vim-indent-guides'},
 }
+
+-- fern
+vim.api.nvim_set_var('fern#renderer', 'nerdfont')
+vim.api.nvim_set_var('fern#renderer#nerdfont#indent_markers', true)
+vim.api.nvim_set_var('fern#default_hidden', 1)
