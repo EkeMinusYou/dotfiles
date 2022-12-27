d415b6b923d67b5d6ef209ab0e29baee82eee9df
EkeMinusYou <euonymuslke@gmail.com>
2022-12-28 06:09:02 +0900

    update editor settings

--- .config/nvim/lua/editor.lua
+++ .config/nvim/lua/editor.lua
@@ -1,5 +1,11 @@
 vim.cmd.colorscheme('tokyonight')
 
-vim.opt.clipboard:append({ unnamedeplus = true })
+vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
