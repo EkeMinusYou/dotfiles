ac3675d2f82a317d9e045eedfe6fbbaacbf9198c
EkeMinusYou <euonymuslke@gmail.com>
2022-12-30 16:42:24 +0900

    enable intro

--- .config/nvim/lua/editor.lua
+++ .config/nvim/lua/editor.lua
@@ -7,4 +7,4 @@ vim.opt.hlsearch = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
 vim.opt.showcmd = false
-vim.opt.shortmess:append "I"
+-- vim.opt.shortmess:append "I"
--- .config/nvim/lua/rc/lualine.lua
+++ .config/nvim/lua/rc/lualine.lua
@@ -2,6 +2,8 @@ require("lualine").setup({
   options = {
     globalstatus = true,
     disabled_filetypes = { 'NvimTree' },
+    section_separators = "",
+    component_separators = ""
   },
 })
 vim.opt.laststatus = 3
