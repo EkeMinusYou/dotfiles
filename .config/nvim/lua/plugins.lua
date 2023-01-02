1bddd674c0f509719b73bced58deb1ff18d33bf6
EkeMinusYou <euonymuslke@gmail.com>
2023-01-02 13:37:58 +0900

    nvim: add null-ls

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,58 +1,62 @@
 vim.cmd.packadd('vim-jetpack')
 
 require('jetpack.packer').add {
-  { 'tani/vim-jetpack', opt = 1 },
+	{ 'tani/vim-jetpack', opt = 1 },
 
-  -- lsp
-  { 'neovim/nvim-lspconfig' },
-  { 'williamboman/mason.nvim' },
-  { 'williamboman/mason-lspconfig.nvim' },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/vim-vsnip" },
+	-- lsp
+	{ 'neovim/nvim-lspconfig' },
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/vim-vsnip" },
+
+	-- formatter
+	{ 'jose-elias-alvarez/null-ls.nvim', requires = { 'nvim-lua/plenary.nvim' } },
 
 	-- coding support
 	{ 'windwp/nvim-autopairs' },
 	{ 'lewis6991/gitsigns.nvim' },
-	{ 'machakann/vim-sandwich'},
+	{ 'machakann/vim-sandwich' },
 	{ 'numToStr/Comment.nvim' },
 	{ 'unblevable/quick-scope' },
 
-  -- treesitter
-  { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
-  { 'nvim-treesitter/nvim-treesitter-context', requires = { 'nvim-treesitter/nvim-treesitter' } },
-  { 'p00f/nvim-ts-rainbow' },
-  { 'lukas-reineke/indent-blankline.nvim', requires = { 'nvim-treesitter/nvim-treesitter' } },
+	-- treesitter
+	{ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+	{ 'nvim-treesitter/nvim-treesitter-context', requires = { 'nvim-treesitter/nvim-treesitter' } },
+	{ 'p00f/nvim-ts-rainbow' },
+	{ 'lukas-reineke/indent-blankline.nvim', requires = { 'nvim-treesitter/nvim-treesitter' } },
 
-  -- explorer
-  { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' } },
-  { 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons' } },
-  { 'nvim-tree/nvim-web-devicons' },
-  { 'nvim-lua/plenary.nvim' },
+	-- explorer
+	{ 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' } },
+	{ 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons' } },
+	{ 'nvim-tree/nvim-web-devicons' },
+	{ 'nvim-lua/plenary.nvim' },
 
-  -- finder
-  { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' }, tag = '0.1.x' },
-  { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+	-- finder
+	{ 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' }, tag = '0.1.x' },
+	{ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
 
-  -- keybinding
-  { 'folke/which-key.nvim' },
+	-- keybinding
+	{ 'folke/which-key.nvim' },
 
-  -- terminal
-  { 'akinsho/toggleterm.nvim' },
+	-- terminal
+	{ 'akinsho/toggleterm.nvim' },
 
 	-- theme
-  { 'folke/tokyonight.nvim' },
-  { 'christianchiarulli/nvcode-color-schemes.vim' },
-  { 'glepnir/zephyr-nvim' },
+	{ 'folke/tokyonight.nvim' },
+	{ 'christianchiarulli/nvcode-color-schemes.vim' },
+	{ 'glepnir/zephyr-nvim' },
 
-  -- others
-  { 'folke/noice.nvim', requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' } },
-  { 'MunifTanjim/nui.nvim' },
-  { 'rcarriga/nvim-notify' },
-	{ 'folke/trouble.nvim', requires = { 'nvim-tree/nvim-web-devicons' }}
+	-- others
+	{ 'folke/noice.nvim', requires = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' } },
+	{ 'MunifTanjim/nui.nvim' },
+	{ 'rcarriga/nvim-notify' },
+	{ 'folke/trouble.nvim', requires = { 'nvim-tree/nvim-web-devicons' } }
 }
 
 require('rc.lsp')
+require('rc.null-ls')
 require('rc.gitsigns')
 require('rc.comment')
 require('rc.quick-scope')
---
+++ .config/nvim/lua/rc/null-ls.lua
@@ -0,0 +1,10 @@
+local null_ls = require('null-ls')
+null_ls.setup {
+	sources = {
+		null_ls.builtins.formatting.eslint,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.gofmt,
+		null_ls.builtins.diagnostics.staticcheck,
+		null_ls.builtins.formatting.lua_format,
+	},
+}
