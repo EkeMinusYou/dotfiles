342ec729df043016e8ec181bdcf2ef99018a0dd5
EkeMinusYou <euonymuslke@gmail.com>
2023-01-03 01:40:02 +0900

    nvim: add lua_format settings

--- .config/nvim/lua/rc/null-ls.lua
+++ .config/nvim/lua/rc/null-ls.lua
@@ -6,6 +6,15 @@ null_ls.setup {
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.diagnostics.staticcheck,
-		null_ls.builtins.formatting.lua_format,
+		null_ls.builtins.formatting.lua_format.with {
+			extra_args = {
+				"indent_width", "2",
+				"use_tab", "false",
+				"continuation_indent_width", "2",
+				"keep_simple_block_one_line", "true",
+				"keep_simple_function_one_line", "true",
+				"align_parameter", "true",
+			},
+		},
 	},
 }
