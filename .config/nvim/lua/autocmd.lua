ba21512d6bb613cbe365ef0c225a619e22f5b829
EkeMinusYou <euonymuslke@gmail.com>
2023-01-05 00:51:02 +0900

    nvim: auto display nvim-tree when VimEnter

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -3,3 +3,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		vim.lsp.buf.format { async = false }
 	end
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.cmd("NvimTreeToggle")
+	end
+})
