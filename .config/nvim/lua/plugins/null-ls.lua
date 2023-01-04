bb25bb78363c80ca1a151e6354cf15e9dd2baa4e
EkeMinusYou <euonymuslke@gmail.com>
2023-01-05 04:47:53 +0900

    nvim/null-ls: fix lua format rules

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,27 +1,25 @@
 return {
-	'jose-elias-alvarez/null-ls.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local null_ls = require('null-ls')
-		null_ls.setup {
-			sources = {
-				null_ls.builtins.formatting.eslint,
-				null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.gofmt,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.diagnostics.staticcheck,
-				null_ls.builtins.formatting.lua_format.with {
-					extra_args = {
-						"indent_width", "2",
-						"use_tab", "false",
-						"continuation_indent_width", "2",
-						"keep_simple_block_one_line", "true",
-						"keep_simple_function_one_line", "true",
-						"align_parameter", "true",
-					},
-				},
-			},
-		}
-	end
+  'jose-elias-alvarez/null-ls.nvim',
+  dependencies = {'nvim-lua/plenary.nvim'},
+  config = function()
+    local null_ls = require('null-ls')
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.diagnostics.staticcheck,
+        null_ls.builtins.formatting.lua_format.with {
+          extra_args = {
+            "--indent-width=2",
+            "--no-use-tab",
+            "--continuation-indent-width=2",
+            "--chop-down-table"
+          }
+        }
+      }
+    }
+  end
 }
