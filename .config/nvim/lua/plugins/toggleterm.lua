f46541fea6ad542e85ce42915996e40aa50fa3c5
EkeMinusYou <euonymuslke@gmail.com>
2023-01-05 05:22:49 +0900

    define k9s and lazygit toggle by row command

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,33 +1,16 @@
-local lazygit
-
-function Toggle_lazygit()
-	lazygit:toggle()
-end
-
-local k9s
-
-function Toggle_k9s()
-	k9s:toggle()
-end
-
 return {
 	'akinsho/toggleterm.nvim',
+	keys = {
+		{
+			"<leader>g",
+			"<CMD>lua require('toggleterm.terminal').Terminal:new({cmd = 'lazygit', hidden = true, direction = 'float'}):toggle()<CR>"
+		},
+		{
+			"<leader>k",
+			"<CMD>lua require('toggleterm.terminal').Terminal:new({cmd = 'k9s --readonly', hidden = true, direction = 'float'}):toggle()<CR>"
+		}
+	},
 	config = function()
 		require("toggleterm").setup()
-
-		-- See https://github.com/akinsho/toggleterm.nvim#custom-terminal-usage
-		local Terminal = require('toggleterm.terminal').Terminal
-
-		k9s = Terminal:new({
-			cmd = "k9s --readonly",
-			hidden = true,
-			direction = "float",
-		})
-
-		lazygit = Terminal:new({
-			cmd = "lazygit",
-			hidden = true,
-			direction = "float"
-		})
 	end
 }
