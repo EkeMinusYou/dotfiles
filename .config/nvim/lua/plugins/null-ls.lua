34ba8b07ef5f2d5a60c70f9684916ee51692f5a9
EkeMinusYou <euonymuslke@gmail.com>
2023-01-05 04:57:36 +0900

    nvim/nul-ls: fix rules

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,25 +1,28 @@
 return {
-  'jose-elias-alvarez/null-ls.nvim',
-  dependencies = {'nvim-lua/plenary.nvim'},
-  config = function()
-    local null_ls = require('null-ls')
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.diagnostics.staticcheck,
-        null_ls.builtins.formatting.lua_format.with {
-          extra_args = {
-            "--indent-width=2",
-            "--no-use-tab",
-            "--continuation-indent-width=2",
-            "--chop-down-table"
-          }
-        }
-      }
-    }
-  end
+	'jose-elias-alvarez/null-ls.nvim',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local null_ls = require('null-ls')
+		null_ls.setup {
+			sources = {
+				null_ls.builtins.formatting.eslint,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.diagnostics.staticcheck,
+				null_ls.builtins.formatting.lua_format.with {
+					extra_args = {
+						"--column-limit=120",
+						"--indent-width=2",
+						"--no-use-tab",
+						"--continuation-indent-width=2",
+						"--chop-down-table",
+						"--no-keep-simple-control-block-one-line",
+						"--no-keep-simple-function-one-line"
+					}
+				}
+			}
+		}
+	end
 }
