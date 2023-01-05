ee6177fd33e71ea327a212c47b103a3be4662d36
EkeMinusYou <euonymuslke@gmail.com>
2023-01-06 00:20:52 +0900

    restrict filetype when format on save

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,11 +1,12 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
-	callback = function()
-		vim.lsp.buf.format { async = false }
-	end,
+vim.api.nvim_create_autocmd('bufwritepre', {
+  pattern = {'*.js', '*.jsx', '*.ts', '*.tsx', '*.go'},
+  callback = function()
+    vim.lsp.buf.format {async = false}
+  end,
 })
 
 vim.api.nvim_create_autocmd('VimEnter', {
-	callback = function()
-		vim.cmd('NvimTreeToggle')
-	end,
+  callback = function()
+    vim.cmd('NvimTreeToggle')
+  end,
 })
