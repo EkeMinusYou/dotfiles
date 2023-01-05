e97c880282892b0e358478593c84b42056c16feb
EkeMinusYou <euonymuslke@gmail.com>
2023-01-06 03:33:17 +0900

    nvim/lsp: fix ensure_installed

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,25 +1,6 @@
 return {
-	'williamboman/mason.nvim',
-	config = function()
-		require('mason').setup({
-			ensured_installed = {
-				'bash-language-server',
-				'dockerfile-language-server',
-				'buf',
-				'buf-language-server',
-				'clang-format',
-				'goimports',
-				'gopls',
-				'json-lsp',
-				'lua-language-server',
-				'luaformatter',
-				'prettier',
-				'tailwindcss-language-server',
-				'terraform-ls',
-				'typescript-language-server',
-				'vim-language-server',
-			},
-			automatic_installation = true,
-		})
-	end,
+  'williamboman/mason.nvim',
+  config = function()
+    require('mason').setup()
+  end,
 }
