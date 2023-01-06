7788a33b5e505e2ecb4677bfe6450028afcde4fb
EkeMinusYou <euonymuslke@gmail.com>
2023-01-07 00:13:22 +0900

    nvim/null-ls: change to faster source

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -6,9 +6,9 @@ return {
     local null_ls = require('null-ls')
     null_ls.setup {
       sources = {
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.prettier_d_slim,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.diagnostics.buf,
