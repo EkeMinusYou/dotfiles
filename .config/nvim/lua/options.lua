dac56e66c543f5a0f889a256881f1ad7cef61ada
EkeMinusYou <euonymuslke@gmail.com>
2023-01-07 03:01:03 +0900

    add options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 vim.opt.clipboard:append 'unnamedplus'
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.list = true
 vim.opt.number = true
 vim.opt.wrap = false
 vim.opt.hlsearch = true
