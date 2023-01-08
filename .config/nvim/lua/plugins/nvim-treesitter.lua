475dd944859c66c60cb783e341fd7370c990218e
EkeMinusYou <euonymuslke@gmail.com>
2023-01-08 15:33:49 +0900

    nvim/treesitter: add incremental_selection

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -40,6 +40,15 @@ return {
     event = 'VeryLazy',
     config = function()
       require('nvim-treesitter.configs').setup({
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+          },
+        },
         textobjects = {
           select = {
             enable = true,
