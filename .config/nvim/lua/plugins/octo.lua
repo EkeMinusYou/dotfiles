30e570ed6353888f1bc59bb40c9667d7ce2c81a8
EkeMinusYou <euonymuslke@gmail.com>
2023-01-11 23:30:30 +0900

    nvim/octo: add gh plugin

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,15 @@
+return {
+  'pwntester/octo.nvim',
+  event = "VeryLazy",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  keys = {
+    { 'gh', '<CMD>Octo actions<CR>' },
+  },
+  config = function()
+    require('octo').setup()
+  end,
+}
