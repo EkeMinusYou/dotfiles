83911f06420ccb29b60221fa26f9063a33533c81
EkeMinusYou <euonymuslke@gmail.com>
2023-01-12 23:52:03 +0900

    nvim/toggleterm: modify keymap

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,8 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<leader>t', '<CMD>ToggleTerm direction=float<CR>' },
-    { '<c-q>', '<CMD>ToggleTerm direction=float<CR>', mode = 't' },
+    { '<leader>t', '<CMD>ToggleTerm direction=horizontal<CR>', mode = {'n', 't'} },
     {
       '<leader>g',
       "<CMD>lua require('toggleterm.terminal').Terminal:new({cmd = 'lazygit', hidden = true, direction = 'float'}):toggle()<CR>",
