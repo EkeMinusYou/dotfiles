15da2a9f336075f325958a16ff81296d1f0dbef2
EkeMinusYou <euonymuslke@gmail.com>
2023-01-13 04:47:02 +0900

    nvim/toggleterm: fix vertical

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<leader>t', '<CMD>ToggleTerm direction=horizontal<CR>', mode = {'n', 't'} },
+    { '<leader>t', '<CMD>ToggleTerm direction=vertical size=80<CR>', mode = {'n', 't'} },
     {
       '<leader>g',
       "<CMD>lua require('toggleterm.terminal').Terminal:new({cmd = 'lazygit', hidden = true, direction = 'float'}):toggle()<CR>",
