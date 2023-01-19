cea36caf8236ef50816277eb24a4ddf6108c2423
EkeMinusYou <euonymuslke@gmail.com>
2023-01-20 04:12:04 +0900

    nvim/tree: add local telescope keymaps

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -6,7 +6,9 @@ return {
     { '<c-q>', '<CMD>ToggleTerm direction=vertical size=80<CR>', mode = 't' },
     {
       '<leader>g',
-      "<CMD>lua require('toggleterm.terminal').Terminal:new({cmd = 'lazygit', hidden = true, direction = 'float'}):toggle()<CR>",
+      function()
+        require('toggleterm.terminal').Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' }):toggle()
+      end,
     },
   },
   config = function()
--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,9 +1,37 @@
+-- See https://github.com/LunarVim/LunarVim/blob/master/lua/lvim/core/nvimtree.lua#L285
+local function start_telescope(telescope_mode)
+  local basedir = vim.fn.getcwd()
+
+  local node = require('nvim-tree.api').tree.get_node_under_cursor()
+  if node ~= nil then
+    local abspath = node.link_to or node.absolute_path
+    local is_folder = node.open ~= nil
+    basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ':h')
+  end
+
+  require('telescope.builtin')[telescope_mode]({
+    cwd = basedir,
+  })
+end
+
 return {
   'nvim-tree/nvim-tree.lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VeryLazy',
   keys = {
-    { '<leader>e', '<CMD>NvimTreeToggle<CR>' },
+    { '<leader>ee', '<CMD>NvimTreeToggle<CR>' },
+    {
+      '<leader>ef',
+      function()
+        start_telescope('find_files')
+      end,
+    },
+    {
+      '<leader>eg',
+      function()
+        start_telescope('live_grep')
+      end,
+    },
   },
   config = function()
     require('nvim-tree').setup({
