a9726aa3e033d82315f1a9e9b9aa235ed04a7df9
EkeMinusYou <euonymuslke@gmail.com>
2023-01-21 04:12:58 +0900

    nvim/treesitter: remove unused move keymaps

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -58,22 +58,6 @@ return {
               ['<leader>A'] = '@parameter.inner',
             },
           },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              [']m'] = '@function.outer',
-            },
-            goto_next_end = {
-              [']M'] = '@function.outer',
-            },
-            goto_previous_start = {
-              ['[m'] = '@function.outer',
-            },
-            goto_previous_end = {
-              ['[M'] = '@function.outer',
-            },
-          },
         },
       })
     end,
