f2537842cf23aa956e5520da04f4a3d47ea3ae7b
EkeMinusYou <euonymuslke@gmail.com>
2023-01-21 18:13:38 +0900

    nvim: increase format timeout

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -23,7 +23,7 @@ vim.keymap.set('t', '<C-S-l>', '<C-\\><C-N><C-w>l')
 
 -- lsp
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.format({async = true, timeout_ms=2000})<CR>')
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
