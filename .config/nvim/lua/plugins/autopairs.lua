94a38a5684dbf69bdd7cac35a757f20f68a740b9
EkeMinusYou <euonymuslke@gmail.com>
2023-01-22 18:56:50 +0900

    nvim/autopairs: enable emacs delete map

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,9 @@ return {
   'windwp/nvim-autopairs',
   event = 'BufReadPost',
   config = function()
-    require('nvim-autopairs').setup()
+    require('nvim-autopairs').setup({
+      map_c_h = true,
+      map_c_w = true,
+    })
   end,
 }
