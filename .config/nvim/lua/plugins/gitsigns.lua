458c77bcd0188cd7841d7ab1174609fce2d17702
EkeMinusYou <euonymuslke@gmail.com>
2023-01-22 17:47:07 +0900

    nvim/git: add line blame

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,8 @@ return {
   'lewis6991/gitsigns.nvim',
   event = 'BufReadPost',
   config = function()
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      current_line_blame = true,
+    })
   end,
 }
