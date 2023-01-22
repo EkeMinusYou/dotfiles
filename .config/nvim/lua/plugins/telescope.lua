5f814fadc64cffe774c39bab17da5d9993f9e564
EkeMinusYou <euonymuslke@gmail.com>
2023-01-22 17:39:56 +0900

    nvim/telescope: enable frecency extension

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,43 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  event = 'VeryLazy',
-  keys = {
-    { '<leader>ff', '<CMD>Telescope find_files<CR>' },
-    { '<leader>fg', '<CMD>Telescope live_grep<CR>' },
-    { '<leader>fb', '<CMD>Telescope buffers<CR>' },
-    { '<leader>fp', '<CMD>Telescope ghq list<CR>' },
-  },
-  config = function()
-    local telescope = require('telescope')
-    telescope.setup({
-      defaults = {
-        file_ignore_patterns = { 'node_modules/', '.git/' },
-      },
-      pickers = {
-        find_files = { hidden = true },
-        live_grep = {
-          additional_args = function()
-            return { '--hidden' }
-          end,
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'nvim-telescope/telescope-frecency.nvim', dependencies = { 'kkharji/sqlite.lua' } },
+    },
+    event = 'VeryLazy',
+    keys = {
+      { '<leader>ff', '<CMD>Telescope frecency workspace=CWD<CR>' },
+      { '<leader>fg', '<CMD>Telescope live_grep<CR>' },
+      { '<leader>fb', '<CMD>Telescope buffers<CR>' },
+      { '<leader>fp', '<CMD>Telescope ghq list<CR>' },
+    },
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = { 'node_modules/', '.git/' },
         },
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = 'smart_case',
+        pickers = {
+          find_files = { hidden = true },
+          live_grep = {
+            additional_args = function()
+              return { '--hidden' }
+            end,
+          },
         },
-      },
-    })
-    telescope.load_extension('fzf')
-  end,
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
+        },
+      })
+      telescope.load_extension('fzf')
+      telescope.load_extension('frecency')
+    end,
+  },
 }
