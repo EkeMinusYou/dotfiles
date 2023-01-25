2640c881859c6c4f211e0746bbc97bd5f67dc12c
EkeMinusYou <euonymuslke@gmail.com>
2023-01-25 23:24:03 +0900

    nvim/lualine: change event VeryLazy -> BufReadPost

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = 'VeryLazy',
+  event = 'BufReadPost',
   config = function()
     vim.opt.laststatus = 3
     require('lualine').setup({
