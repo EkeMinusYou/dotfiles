bae127282f81108a2f5ff5696e15a2f029249fdb
EkeMinusYou <euonymuslke@gmail.com>
2023-02-03 00:38:52 +0900

    neovim/cmp: enable cmdline

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -12,6 +12,12 @@ return {
   config = function()
     local cmp = require('cmp')
     local lspkind = require('lspkind')
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'cmdline' },
+      },
+    })
     cmp.setup.cmdline('/', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
