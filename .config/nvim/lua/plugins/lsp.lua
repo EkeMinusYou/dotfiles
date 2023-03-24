388777c5f282cdfdc57cea8c632e0358c5e93e83
EkeMinusYou <euonymuslke@gmail.com>
2023-03-25 02:39:54 +0900

    nvim/lua_ls: CallSnippet = 'Replace'

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -39,7 +39,20 @@ return {
 
       require('mason-lspconfig').setup_handlers({
         function(server_name)
-          require('lspconfig')[server_name].setup({})
+          local lspconfig = require('lspconfig')
+          if server_name == 'lua_ls' then
+            lspconfig.lua_ls.setup({
+              settings = {
+                Lua = {
+                  completion = {
+                    callSnippet = 'Replace',
+                  },
+                },
+              },
+            })
+          else
+            lspconfig[server_name].setup({})
+          end
         end,
       })
     end,
