b232025b9af3960d15fe723a5110541c908e5445
EkeMinusYou <euonymuslke@gmail.com>
2023-04-14 03:17:32 +0900

    null-ls: judge condition by local directory

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,24 @@
+local function exists_in_dir(filename, dir)
+  local files = vim.fn.readdir(dir)
+  return vim.tbl_contains(files, filename)
+end
+
+local function local_has_file(filenames, levels_up)
+  if levels_up == nil then
+    levels_up = 5
+  end
+  local dir = vim.fn.expand('%:p:h')
+  for i = 1, levels_up + 1 do
+    for _, filename in ipairs(filenames) do
+      if exists_in_dir(filename, dir) then
+        return true
+      end
+    end
+    dir = vim.fn.fnamemodify(dir, ':h')
+  end
+  return false
+end
+
 return {
   'jose-elias-alvarez/null-ls.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
@@ -9,22 +30,22 @@ return {
         -- js,ts
         null_ls.builtins.formatting.prettierd.with({
           condition = function(utils)
-            return utils.root_has_file({ '.prettierrc' })
+            return local_has_file({ '.prettierrc' })
           end,
         }),
         null_ls.builtins.formatting.eslint_d.with({
           condition = function(utils)
-            return utils.root_has_file({ '.eslintrc.js', '.eslintrc.cjs' })
+            return local_has_file({ '.eslintrc.js', '.eslintrc.cjs' })
           end,
         }),
         null_ls.builtins.formatting.rome.with({
           condition = function(utils)
-            return utils.root_has_file({ 'rome.json' })
+            return local_has_file({ 'rome.json' })
           end,
         }),
         null_ls.builtins.diagnostics.eslint_d.with({
           condition = function(utils)
-            return utils.root_has_file({ '.eslintrc.js', '.eslintrc.cjs' })
+            return local_has_file({ '.eslintrc.js', '.eslintrc.cjs' })
           end,
           diagnostics_format = '[eslint] #{m}\n(#{c})',
         }),
