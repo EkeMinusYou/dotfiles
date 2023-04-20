a8755a35dcaeb9d9733471321e16cdb84066c507
EkeMinusYou <euonymuslke@gmail.com>
2023-04-21 04:48:39 +0900

    wezterm: disable opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require("wezterm")
 return {
 	color_scheme = "OneHalfDark",
-	window_background_opacity = 0.93,
 	window_close_confirmation = "NeverPrompt",
 	use_ime = true,
 	font = wezterm.font("HackGen35 Console NFJ"),
