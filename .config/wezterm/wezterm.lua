927dd38b3097d9de6410012e4d2136eb48c0a39c
EkeMinusYou <euonymuslke@gmail.com>
2023-04-20 23:51:29 +0900

    wezterm: update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require("wezterm")
 return {
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = "OneHalfDark",
 	window_background_opacity = 0.93,
+	window_close_confirmation = "NeverPrompt",
+	use_ime = true,
 	font = wezterm.font("HackGen35 Console NFJ"),
+	font_size = 13.0,
 }
