d7b5fcbf8e6142ba2ee330a642a89218ae3e578f
EkeMinusYou <euonymuslke@gmail.com>
2023-04-23 03:26:06 +0900

    wezterm: fullscreen on startup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,17 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():toggle_fullscreen()
+end)
+
 return {
 	color_scheme = "OneHalfDark",
 	window_close_confirmation = "NeverPrompt",
+	native_macos_fullscreen_mode = true,
+	hide_tab_bar_if_only_one_tab = true,
 	use_ime = true,
 	font = wezterm.font("HackGen35 Console NFJ"),
-	font_size = 13.0,
+	font_size = 16.0,
 }
