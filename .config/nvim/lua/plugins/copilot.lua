d76258e962c1ee7816119c90c680061f333caa62
EkeMinusYou <euonymuslke@gmail.com>
2023-05-11 23:27:35 +0900

    nvim/copilot: replace copilot plugin

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,12 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
   event = 'InsertEnter',
   config = function()
+    require('copilot').setup({
+      suggestion = {
+        auto_trigger = true,
+      },
+    })
   end,
 }
