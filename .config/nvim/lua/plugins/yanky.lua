5e0fca21802e3e7d4234089207c46aa97c4c8ae5
EkeMinusYou <euonymuslke@gmail.com>
2023-06-18 22:53:36 +0900

    nvim: install yanky

---
+++ .config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,12 @@
+return {
+  'gbprod/yanky.nvim',
+  keys = {
+    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' } },
+    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' } },
+    { '<c-n>', '<Plug>(YankyCycleForward)', mode = { 'n', 'x' } },
+    { '<c-p>', '<Plug>(YankyCycleBackward)', mode = { 'n', 'x' } },
+  },
+  config = function()
+    require('yanky').setup()
+  end,
+}
