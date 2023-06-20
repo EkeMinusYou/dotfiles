2ead76501059e4a0b3a4f06daa71a5506e3f2c51
EkeMinusYou <euonymuslke@gmail.com>
2023-06-21 03:22:43 +0900

    wezterm: add pane controll keymaps

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function()
@@ -12,6 +13,27 @@ return {
 	native_macos_fullscreen_mode = true,
 	hide_tab_bar_if_only_one_tab = true,
 	use_ime = true,
+
 	font = wezterm.font("HackGen35 Console NF"),
 	font_size = 13.0,
+
+	keys = {
+		{ key = "n", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(1) },
+		{ key = "p", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
+
+		{ key = "v", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "s", mods = "CTRL|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+		{ key = "x", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+
+		{ key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+		{ key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+
+		{ key = "h", mods = "CTRL|SHIFT|ALT", action = act.AdjustPaneSize({ "Left", 10 }) },
+		{ key = "l", mods = "CTRL|SHIFT|ALT", action = act.AdjustPaneSize({ "Right", 10 }) },
+		{ key = "k", mods = "CTRL|SHIFT|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
+		{ key = "j", mods = "CTRL|SHIFT|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	},
 }
