cc4d94bbc7cd181879af52440a41a684e8d81762
EkeMinusYou <euonymuslke@gmail.com>
2023-07-08 04:13:23 +0900

    nvim/copilot: ignore M-l for yabai/skhd

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -5,7 +5,17 @@ return {
   config = function()
     require('copilot').setup({
       suggestion = {
+        enabled = true,
         auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = '<M-:>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
       },
     })
   end,
