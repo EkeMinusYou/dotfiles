00eea2b6056fa03c75733c3455b8a781a426ab7f
EkeMinusYou <euonymuslke@gmail.com>
2023-07-08 01:12:38 +0900

    nvim/surroud: release s key

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,7 +1,20 @@
 return {
   'echasnovski/mini.surround',
-  event = 'BufReadPost',
+  event = 'VeryLazy',
   config = function()
-    require('mini.surround').setup({})
+    require('mini.surround').setup({
+      mappings = {
+        add = 'ys',
+        delete = 'ds',
+        find = '',
+        find_left = '',
+        highlight = '',
+        replace = 'rs',
+        update_n_lines = '',
+
+        suffix_last = '',
+        suffix_next = '',
+      },
+    })
   end,
 }
