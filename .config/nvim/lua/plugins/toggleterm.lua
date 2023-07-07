c166f5996b23786de42becdba12b4143f50971fa
EkeMinusYou <euonymuslke@gmail.com>
2023-07-07 23:34:01 +0900

    nvim: do not use terminal in nvim

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,8 +2,6 @@ return {
   'akinsho/toggleterm.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<leader>t', '<cmd>ToggleTerm direction=horizontal size=15<cr>' },
-    { '<c-q>', '<cmd>ToggleTerm direction=horizontal size=15<cr>', mode = 't' },
     {
       '<leader>g',
       function()
