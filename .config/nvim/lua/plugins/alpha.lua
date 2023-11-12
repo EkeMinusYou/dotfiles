337ba28a905d70c00a23f8101d1bce3f002fc8e4
EkeMinusYou <euonymuslke@gmail.com>
2023-11-13 03:23:02 +0900

    neovim/alpha: remove new file
    fix

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,6 +1,10 @@
 return {
   'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('alpha').setup(require('alpha.themes.startify').config)
+    local alpha = require('alpha')
+    local startify = require('alpha.themes.startify')
+    startify.section.top_buttons.val = {}
+    alpha.setup(startify.config)
   end,
 }
