7c91395b83ce8cff4703258059bffc63626fbd49
EkeMinusYou <euonymuslke@gmail.com>
2023-12-24 15:28:12 +0900

    nvim/telescope: update live-grep-args config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
   },
   config = function()
     local telescope = require('telescope')
+    local lga_actions = require('telescope-live-grep-args.actions')
     telescope.setup({
       defaults = {
         file_ignore_patterns = {
@@ -38,6 +39,27 @@ return {
           override_generic_sorter = true,
           override_file_sorter = true,
           case_mode = 'smart_case',
+        },
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ['<C-k>'] = lga_actions.quote_prompt(),
+              ['<C-i>'] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
+            },
+          },
+          vimgrep_arguments = {
+            {
+              'rg',
+              '--hidden',
+              '--color=never',
+              '--no-heading',
+              '--with-filename',
+              '--line-number',
+              '--column',
+              '--smart-case',
+            },
+          },
         },
       },
     })
