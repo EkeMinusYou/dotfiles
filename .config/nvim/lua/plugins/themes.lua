17a96d5e74e69ea3166ced33db7542da17d8b265
EkeMinusYou <euonymuslke@gmail.com>
2023-12-25 04:43:02 +0900

    nvim/theme: enable kanagawa-wave

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,14 +1,14 @@
 return {
   {
     'rebelot/kanagawa.nvim',
-    lazy = true,
+    lazy = false,
     config = function()
       vim.cmd([[colorscheme kanagawa-wave]])
     end,
   },
   {
     'folke/tokyonight.nvim',
-    lazy = false,
+    lazy = true,
     config = function()
       vim.cmd([[colorscheme tokyonight-night]])
     end,
