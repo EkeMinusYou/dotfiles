318800fe5fe584169a060085b059a3ae766c569d
EkeMinusYou <euonymuslke@gmail.com>
2024-01-23 03:49:58 +0900

    nvim/skkeleton: add dictionaries

--- .config/nvim/lua/plugins/skkeleton.lua
+++ .config/nvim/lua/plugins/skkeleton.lua
@@ -1,10 +1,28 @@
 return {
-  'vim-skk/skkeleton',
-  event = 'VeryLazy',
-  keys = {
-    { '<c-j>', '<Plug>(skkeleton-enable)', mode = { 'i', 'c' } },
+  {
+    'vim-skk/skkeleton',
+    event = 'VeryLazy',
+    keys = {
+      { '<c-j>', '<Plug>(skkeleton-enable)', mode = { 'i', 'c' } },
+    },
+    dependencies = {
+      'vim-denops/denops.vim',
+    },
+    config = function()
+      local dict = vim.fn.stdpath('data') .. '/lazy/dict'
+      vim.fn['skkeleton#config']({
+        globalDictionaries = {
+          dict .. '/SKK-JISYO.L',
+          dict .. '/SKK-JISYO.jinmei',
+          dict .. '/SKK-JISYO.geo',
+          dict .. '/SKK-JISYO.station',
+          dict .. '/SKK-JISYO.propernoun',
+        },
+      })
+    end,
   },
-  dependencies = {
-    'vim-denops/denops.vim',
+  {
+    'skk-dev/dict',
+    event = 'VeryLazy',
   },
 }
