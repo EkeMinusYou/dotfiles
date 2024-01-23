4c5e725c473e5cfff6147a48f1a61e78c3bdbb4a
EkeMinusYou <euonymuslke@gmail.com>
2024-01-24 03:09:32 +0900

    nvim: add filetype autocmd

---
+++ .config/nvim/lua/filetype.lua
@@ -0,0 +1,39 @@
+vim.api.nvim_create_augroup('filetype', {})
+
+-- Git
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '.gitconfig-*',
+  group = 'filetype',
+  command = 'setlocal filetype=gitconfig',
+})
+
+-- Swift
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = 'Package.resolved',
+  group = 'filetype',
+  command = 'setlocal filetype=json',
+})
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.xcstrings',
+  group = 'filetype',
+  command = 'setlocal filetype=json',
+})
+
+-- CocoaPods
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.podspec',
+  group = 'filetype',
+  command = 'setlocal filetype=ruby',
+})
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = 'Podfile',
+  group = 'filetype',
+  command = 'setlocal filetype=ruby',
+})
+
+-- brewfile
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = 'Brewfile',
+  group = 'filetype',
+  command = 'setlocal filetype=ruby',
+})
