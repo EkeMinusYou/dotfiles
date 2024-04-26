835c9270642056cd29ae17ad89204d6110449e23
EkeMinusYou <euonymuslke@gmail.com>
2024-04-26 10:52:41 +0900

    nvim/mini: remove animate

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -20,30 +20,6 @@ return {
     end,
   },
   {
-    'echasnovski/mini.animate',
-    event = 'VeryLazy',
-    config = function()
-      local animate = require('mini.animate')
-      animate.setup({
-        scroll = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-        },
-        cursor = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-        },
-        resize = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-        },
-        open = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-        },
-        close = {
-          timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
-        },
-      })
-    end,
-  },
-  {
     'echasnovski/mini.comment',
     event = 'BufReadPost',
     config = function()
