5294d2e208f3e38a2ad53b603d8e05cfa28e76ec
EkeMinusYou <euonymuslke@gmail.com>
2024-05-15 03:52:33 +0900

    wezterm: padding 0

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,6 +12,8 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	use_ime = true,
 
+	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+
 	font = wezterm.font("HackGen35 Console NF"),
 	font_size = 20.0,
 
