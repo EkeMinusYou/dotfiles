f56127f5b279a03ce4dd80b492fe84d457762238
EkeMinusYou <euonymuslke@gmail.com>
2024-05-17 13:53:25 +0900

    nvim/snippets: add keys

--- .config/nvim/lua/plugins/snippet.lua
+++ .config/nvim/lua/plugins/snippet.lua
@@ -9,6 +9,31 @@ return {
     local snippetsDir = vim.fn.stdpath('data') .. '/lazy/friendly-snippets/snippets'
     table.insert(dirs, snippetsDir)
     vim.g.vsnip_snippet_dirs = dirs
+
+    vim.api.nvim_set_keymap(
+      'i',
+      '<Tab>',
+      "vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
+      { expr = true, noremap = false }
+    )
+    vim.api.nvim_set_keymap(
+      's',
+      '<Tab>',
+      "vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
+      { expr = true, noremap = false }
+    )
+    vim.api.nvim_set_keymap(
+      'i',
+      '<S-Tab>',
+      "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'",
+      { expr = true, noremap = false }
+    )
+    vim.api.nvim_set_keymap(
+      's',
+      '<S-Tab>',
+      "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'",
+      { expr = true, noremap = false }
+    )
   end,
   lazy = true,
 }
