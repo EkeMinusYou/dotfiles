55c7bf773d6a4b481e7d4adeb62265137e0fb9a2
EkeMinusYou <euonymuslke@gmail.com>
2024-05-19 06:08:29 +0900

    nvim/noice: top_down = false

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,29 +1,39 @@
 return {
-  'folke/noice.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
-  event = 'VeryLazy',
-  config = function()
-    -- See https://github.com/folke/noice.nvim#-installation
-    require('noice').setup({
-      messages = {
-        view = 'mini',
-      },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      require('notify').setup({
+        top_down = false,
+      })
+    end,
+  },
+  {
+    'folke/noice.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
+    event = 'VeryLazy',
+    config = function()
+      -- See https://github.com/folke/noice.nvim#-installation
+      require('noice').setup({
+        messages = {
+          view = 'mini',
         },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    })
-  end,
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
 }
