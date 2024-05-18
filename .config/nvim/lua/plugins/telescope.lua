35edb38d9a3ed7f712f13120af539983dbf27106
EkeMinusYou <euonymuslke@gmail.com>
2024-05-19 05:01:19 +0900

    nvim/telescope: add all-recent
    add

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -55,105 +55,119 @@ local function ghq_list(opts)
 end
 
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'fdschmidt93/telescope-egrepify.nvim' },
-    { 'nvim-telescope/telescope-frecency.nvim' },
-    { 'stevearc/aerial.nvim' },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-    { 'jonarrien/telescope-cmdline.nvim' }, -- experimental
-  },
-  event = 'VeryLazy',
-  cmd = {
-    'Telescope',
-  },
-  keys = {
-    { '<leader>ff', '<cmd>Telescope find_files<cr>' },
-    { '<leader>fr', '<cmd>Telescope frecency workspace=CWD<cr>' },
-    {
-      '<leader>fg',
-      function()
-        require('telescope').extensions.egrepify.egrepify()
-      end,
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { 'fdschmidt93/telescope-egrepify.nvim' },
+      { 'nvim-telescope/telescope-frecency.nvim' },
+      { 'stevearc/aerial.nvim' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'jonarrien/telescope-cmdline.nvim' }, -- experimental
     },
-    { '<leader>fk', grep_by_kensaku },
-    { '<leader>fb', '<cmd>Telescope buffers<cr>' },
-    { '<leader>fs', '<cmd>Telescope git_status<cr>' },
-    { '<leader>fo', '<cmd>Telescope aerial<cr>' },
-    -- { ':', '<cmd>Telescope cmdline<cr>' }, -- experimental
-    {
-      '<leader>fm',
-      function()
-        ghq_list()
-      end,
+    event = 'VeryLazy',
+    cmd = {
+      'Telescope',
     },
-  },
-  config = function()
-    local telescope = require('telescope')
-    telescope.setup({
-      defaults = {
-        path_display = {
-          'filename_first',
-        },
-        file_ignore_patterns = {
-          'node_modules/',
-          '.git/',
-          '%.pb.go',
-          '%.gen.go',
-          '%.gw.go',
-        },
-        sorting_strategy = 'ascending',
-        layout_strategy = 'horizontal',
-        layout_config = { prompt_position = 'top' },
-        border = true,
+    keys = {
+      { '<leader>ff', '<cmd>Telescope find_files<cr>' },
+      { '<leader>fr', '<cmd>Telescope frecency workspace=CWD<cr>' },
+      {
+        '<leader>fg',
+        function()
+          require('telescope').extensions.egrepify.egrepify()
+        end,
       },
-      pickers = {
-        find_files = { hidden = true },
-        live_grep = {
-          hidden = true,
-        },
+      { '<leader>fk', grep_by_kensaku },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>' },
+      { '<leader>fs', '<cmd>Telescope git_status<cr>' },
+      { '<leader>fo', '<cmd>Telescope aerial<cr>' },
+      -- { ':', '<cmd>Telescope cmdline<cr>' }, -- experimental
+      {
+        '<leader>fm',
+        function()
+          ghq_list()
+        end,
       },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = 'smart_case',
+    },
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        defaults = {
+          path_display = {
+            'filename_first',
+          },
+          file_ignore_patterns = {
+            'node_modules/',
+            '.git/',
+            '%.pb.go',
+            '%.gen.go',
+            '%.gw.go',
+          },
+          sorting_strategy = 'ascending',
+          layout_strategy = 'horizontal',
+          layout_config = { prompt_position = 'top' },
+          border = true,
         },
-        frecency = {
-          db_safe_mode = false,
+        pickers = {
+          find_files = { hidden = true },
+          live_grep = {
+            hidden = true,
+          },
         },
-        egrepify = {
-          vimgrep_arguments = {
-            {
-              'rg',
-              '--color=never',
-              '--no-heading',
-              '--with-filename',
-              '--line-number',
-              '--column',
-              '--smart-case',
-              '--hidden',
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
+          frecency = {
+            db_safe_mode = false,
+          },
+          egrepify = {
+            vimgrep_arguments = {
+              {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case',
+                '--hidden',
+              },
+            },
+          },
+          cmdline = {
+            mappings = {
+              complete = '<tab>',
+              run_selection = '<cr>',
+              run_input = '<c-cr>',
             },
           },
         },
-        cmdline = {
-          mappings = {
-            complete = '<tab>',
-            run_selection = '<cr>',
-            run_input = '<c-cr>',
-          },
-        },
-      },
-    })
+      })
 
-    telescope.load_extension('fzf')
-    telescope.load_extension('egrepify')
-    telescope.load_extension('frecency')
-    telescope.load_extension('cmdline')
-    telescope.load_extension('aerial')
-    telescope.load_extension('ui-select')
-  end,
+      telescope.load_extension('fzf')
+      telescope.load_extension('egrepify')
+      telescope.load_extension('frecency')
+      telescope.load_extension('cmdline')
+      telescope.load_extension('aerial')
+      telescope.load_extension('ui-select')
+    end,
+  },
+  {
+    'prochri/telescope-all-recent.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'kkharji/sqlite.lua',
+      'stevearc/dressing.nvim',
+    },
+    config = function()
+      require('telescope-all-recent').setup({})
+    end,
+  },
 }
