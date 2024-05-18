b87069f37bfae4f8295b30709370b791c47e6c65
EkeMinusYou <euonymuslke@gmail.com>
2024-05-19 05:40:46 +0900

    nvim/lazy: fix setup params

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,11 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup({ import = 'plugins' }, {
+  change_detection = {
+    notify = false,
+  },
+})
