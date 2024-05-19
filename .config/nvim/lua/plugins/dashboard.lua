e915e9d04dc2e21ec8939a8aa39905af27bb8843
EkeMinusYou <euonymuslke@gmail.com>
2024-05-19 19:40:55 +0900

    nvim: repalce dashboard plugin

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,14 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  config = function()
+    require('dashboard').setup({
+      config = {
+        week_header = {
+          enable = true,
+        },
+      },
+    })
+  end,
+}
