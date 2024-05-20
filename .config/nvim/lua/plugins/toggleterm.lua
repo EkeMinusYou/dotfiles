b02c95a7a08a97ec58d5cf7ceb226f86da0c229b
EkeMinusYou <euonymuslke@gmail.com>
2024-05-21 01:50:17 +0900

    nvim/telescope: every create lazygit

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,24 +1,22 @@
-local lazygit
-
-return {
-  'akinsho/toggleterm.nvim',
-  lazy = true,
-  keys = {
-    {
-      '<leader>g',
-      function()
-        lazygit:toggle()
-      end,
-    },
-  },
-  config = function()
-    require('toggleterm').setup()
-    local terminal = require('toggleterm.terminal').Terminal
-    lazygit = terminal:new({
+local function lazygit()
+  local terminal = require('toggleterm.terminal').Terminal
+  terminal
+    :new({
       cmd = 'lazygit',
       dir = 'git_dir',
       hidden = true,
       direction = 'float',
     })
+    :toggle()
+end
+
+return {
+  'akinsho/toggleterm.nvim',
+  lazy = true,
+  keys = {
+    { '<leader>g', lazygit },
+  },
+  config = function()
+    require('toggleterm').setup()
   end,
 }
