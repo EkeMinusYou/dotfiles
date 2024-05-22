fd0245e2cb104433effd66378fd99bff4620486b
EkeMinusYou <euonymuslke@gmail.com>
2024-05-22 14:27:57 +0900

    nvim/dashboard: add restore keymap

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,7 +1,15 @@
+local function restore()
+  require('nvim-tree.api').tree.close()
+  require('dashboard'):instance()
+end
+
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '<leader>x', restore },
+  },
   config = function()
     require('dashboard').setup({
       config = {
