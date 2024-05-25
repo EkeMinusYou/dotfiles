4253aa2ff58d23549fb0f0f8c79b247a2195bc64
EkeMinusYou <euonymuslke@gmail.com>
2024-05-26 02:02:26 +0900

    telescope/ghq: add optional keymap
    fix

--- .config/nvim/lua/internal/telescope-ghq.nvim/lua/telescope/_extensions/ghq.lua
+++ .config/nvim/lua/internal/telescope-ghq.nvim/lua/telescope/_extensions/ghq.lua
@@ -3,7 +3,6 @@ local function ghq_list(opts)
   local finders = require('telescope.finders')
   local conf = require('telescope.config').values
   local actions = require('telescope.actions')
-  local actions_set = require('telescope.actions.set')
   local actions_state = require('telescope.actions.state')
   local from_entry = require('telescope.from_entry')
 
@@ -26,17 +25,22 @@ local function ghq_list(opts)
       finder = finders.new_oneshot_job({ 'ghq', 'list', '--full-path' }, opts),
       previewer = conf.file_previewer(opts),
       sorter = conf.file_sorter(opts),
-      attach_mappings = function(prompt_bufnr)
-        ---@diagnostic disable-next-line: undefined-field
-        actions_set.select:replace(function(_, type)
+      attach_mappings = function(prompt_bufnr, map)
+        map('i', '<c-f>', function()
           local entry = actions_state.get_selected_entry()
           local dir = from_entry.path(entry)
-          if type == 'default' then
-            vim.cmd('cd ' .. dir)
-            require('telescope.builtin').find_files({ cwd = dir })
-            return
-          end
+          require('telescope.builtin').find_files({ cwd = dir })
+        end)
+        map('i', '<c-g>', function()
+          local entry = actions_state.get_selected_entry()
+          local dir = from_entry.path(entry)
+          require('telescope').extensions.egrepify.egrepify({ cwd = dir })
+        end)
+        map('i', '<cr>', function()
+          local entry = actions_state.get_selected_entry()
+          local dir = from_entry.path(entry)
           actions.close(prompt_bufnr)
+          vim.api.nvim_command('edit ' .. dir .. '/' .. 'README.md')
         end)
         return true
       end,
