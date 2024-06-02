96f08c40af379a504a7e3837e53e6d8adaf7a897
EkeMinusYou <euonymuslke@gmail.com>
2024-06-02 23:58:50 +0900

    nvim/treesitter: disable warning

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     build = ':TSUpdate',
     event = 'BufReadPost',
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup({
         ensure_installed = {
           'javascript',
