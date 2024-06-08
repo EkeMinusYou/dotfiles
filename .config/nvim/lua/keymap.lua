84f813ee3c78c84e8fbd2ffc5cec508f1a36ff52
EkeMinusYou <euonymuslke@gmail.com>
2024-06-08 23:52:21 +0900

    nvim: add keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -40,3 +40,8 @@ vim.keymap.set('c', '<CR>', '<Plug>(kensaku-search-replace)<CR>')
 
 -- terminal
 -- vim.keymap.set('n', '<leader>g', '<cmd>terminal lazygit<cr>')
+
+-- See: https://zenn.dev/vim_jp/articles/2024-06-05-vim-middle-class-features#%E5%BC%95%E7%94%A8%E7%AC%A6%E3%81%A7%E5%9B%B2%E3%81%BE%E3%82%8C%E3%81%9F%E7%AE%87%E6%89%80%E5%85%A8%E4%BD%93%E3%82%92%E9%81%B8%E6%8A%9E%E3%81%99%E3%82%8B
+for _, quote in ipairs({ '"', "'", '`' }) do
+  vim.keymap.set({ 'x', 'o' }, 'a' .. quote, '2i' .. quote)
+end
