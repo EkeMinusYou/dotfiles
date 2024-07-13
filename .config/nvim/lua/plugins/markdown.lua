3530ac3d1fd970f1099f232634d5690a485bf9ef
EkeMinusYou <euonymuslke@gmail.com>
2024-07-13 22:54:02 +0900

    nvim: added markdown.nvim

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  'MeanderingProgrammer/markdown.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('render-markdown').setup({})
+  end,
+}
