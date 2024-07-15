91b6c13cf9eb34e6a21f7dee86e9b799c12b127f
EkeMinusYou <euonymuslke@gmail.com>
2024-07-15 16:40:36 +0900

    nvim/xcodebuild: disable nvim_tree integration, i use xcodegen

--- .config/nvim/lua/plugins/xcodebuild.lua
+++ .config/nvim/lua/plugins/xcodebuild.lua
@@ -4,10 +4,13 @@ return {
   dependencies = {
     'nvim-telescope/telescope.nvim',
     'MunifTanjim/nui.nvim',
-    'nvim-tree/nvim-tree.lua',
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('xcodebuild').setup({})
+    require('xcodebuild').setup({
+      nvim_tree = {
+        enabled = false,
+      },
+    })
   end,
 }
