3b33eb7df006eb4c4d324a037e51282d63ec004b
EkeMinusYou <euonymuslke@gmail.com>
2024-07-18 02:55:16 +0900

    nvim: remove lsp_all_stop

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -17,20 +17,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
-local function lsp_all_stop()
-  for _, client in pairs(vim.lsp.get_clients()) do
-    client.stop()
-  end
-end
-
-vim.api.nvim_create_augroup('lsp', {})
-vim.api.nvim_create_autocmd('DirChangedPre', {
-  group = 'lsp',
-  callback = function()
-    lsp_all_stop()
-  end,
-})
-
 -- denolscache
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = 'format',
