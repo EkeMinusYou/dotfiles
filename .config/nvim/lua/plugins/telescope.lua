d93209ca9af983228aa052e430797cacdffe8a0c
EkeMinusYou <euonymuslke@gmail.com>
2024-09-04 04:56:13 +0900

    telescope: add history cycle keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -137,8 +137,15 @@ return {
     },
     config = function()
       local telescope = require('telescope')
+      local actions = require('telescope.actions')
       telescope.setup({
         defaults = {
+          mappings = {
+            i = {
+              ['<C-j>'] = actions.cycle_history_next,
+              ['<C-k>'] = actions.cycle_history_prev,
+            },
+          },
           path_display = {
             'filename_first',
           },
