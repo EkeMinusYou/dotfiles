671469af5f9458f608f63ab3e536f5dad60d545a
EkeMinusYou <euonymuslke@gmail.com>
2024-09-18 20:28:12 +0900

    nvim/tree: disable hijack_cursor

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -53,7 +53,6 @@ return {
       require('nvim-tree').setup({
         on_attach = on_attach,
         sort_by = 'extension',
-        hijack_cursor = true,
         renderer = {
           highlight_opened_files = 'name',
           highlight_diagnostics = 'all',
