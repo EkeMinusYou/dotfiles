48e49262bad25c252cca1607afaee21bfdbb21f9
EkeMinusYou <euonymuslke@gmail.com>
2024-10-27 19:11:43 +0900

    nvim: added nvim-highlight-colors
    change

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,4 +1,15 @@
 return {
   { 'nvchad/volt', lazy = true },
   { 'nvchad/minty', lazy = true },
+  {
+    'brenoprata10/nvim-highlight-colors',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+    config = function()
+      require('nvim-highlight-colors').setup({
+        render = 'virtual',
+        virtual_symbol_suffix = '',
+        enable_tailwind = true,
+      })
+    end,
+  },
 }
