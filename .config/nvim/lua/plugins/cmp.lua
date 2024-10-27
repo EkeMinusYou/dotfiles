fc7801a026f009d736c166f284b7fb6109270ff9
EkeMinusYou <euonymuslke@gmail.com>
2024-10-27 19:25:45 +0900

    nvim/color: add cmp settings

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -68,9 +68,17 @@ return {
       }),
       ---@diagnostic disable-next-line: missing-fields
       formatting = {
-        format = lspkind.cmp_format({
-          mode = 'symbol_text',
-        }),
+        format = function(entry, item)
+          local color_item = require('nvim-highlight-colors').format(entry, { kind = item.kind })
+          item = require('lspkind').cmp_format({
+            mode = 'symbol_text',
+          })(entry, item)
+          if color_item.abbr_hl_group then
+            item.kind_hl_group = color_item.abbr_hl_group
+            item.kind = color_item.abbr
+          end
+          return item
+        end,
       },
       ---@diagnostic disable-next-line: missing-fields
       performance = {
