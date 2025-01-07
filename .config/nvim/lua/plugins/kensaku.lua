3a11cafb7e73f83fcab6985da8656b73b42bcaa1
EkeMinusYou <euonymuslke@gmail.com>
2025-01-07 10:36:51 +0900

    nvim: added fuzy-motion.vim

--- .config/nvim/lua/plugins/kensaku.lua
+++ .config/nvim/lua/plugins/kensaku.lua
@@ -13,4 +13,15 @@ return {
     },
     event = 'VeryLazy',
   },
+  {
+    'yuki-yano/fuzzy-motion.vim',
+    dependencies = {
+      'vim-denops/denops.vim',
+      'lambdalisue/kensaku.vim',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader><leader>', '<cmd>FuzzyMotion<cr>', { noremap = true, silent = true })
+      vim.cmd("let g:fuzzy_motion_matchers = ['kensaku', 'fzf']")
+    end,
+  },
 }
