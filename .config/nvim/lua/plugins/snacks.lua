62781bd8f72558678b3600bd561805b1c4a8e0b6
EkeMinusYou <euonymuslke@gmail.com>
2025-01-15 15:56:38 +0900

    nvim/snacks: simplify settings

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,21 +2,7 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
-  opts = {
-    bigfile = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
-  },
-  -- keys = {
-  --   {
-  --     '<leader>gg',
-  --     function()
-  --       Snacks.lazygit()
-  --     end,
-  --     desc = 'Lazygit',
-  --   },
-  -- },
+  config = function()
+    require('snacks').setup({})
+  end,
 }
