aeb53543b8ecae0faa2056191ffcbcde1a4529a7
EkeMinusYou <euonymuslke@gmail.com>
2025-03-08 23:25:02 +0900

    Revert "nvim: remove skkeleton"
    This reverts commit ef780446d3f17f15396a9f09be598d97e2f41557.

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -12,6 +12,7 @@ return {
     { 'hrsh7th/vim-vsnip-integ' },
     { 'hrsh7th/vim-vsnip' },
     { 'onsails/lspkind.nvim' },
+    { 'rinx/cmp-skkeleton', dependencies = { 'vim-skk/skkeleton' } },
     { 'folke/lazydev.nvim' },
   },
   config = function()
@@ -40,6 +41,7 @@ return {
         end,
       },
       sources = cmp.config.sources({
+        { name = 'skkeleton' },
         { name = 'nvim_lsp', keyword_length = 1 },
         { name = 'vsnip', keyword_length = 1 },
         { name = 'nvim_lsp_signature_help' },
