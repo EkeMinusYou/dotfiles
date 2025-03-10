ec5cc14e7db34ac6287903733ea7456469c90e68
EkeMinusYou <euonymuslke@gmail.com>
2025-03-10 21:52:14 +0900

    skkeleton: disable

--- .config/nvim/lua/plugins/skkeleton.lua
+++ .config/nvim/lua/plugins/skkeleton.lua
@@ -3,7 +3,7 @@ return {
     'vim-skk/skkeleton',
     event = 'VeryLazy',
     keys = {
-      { '<c-j>', '<Plug>(skkeleton-enable)', mode = { 'i', 'c' } },
+      -- { '<c-j>', '<Plug>(skkeleton-enable)', mode = { 'i', 'c' } },
     },
     dependencies = {
       'vim-denops/denops.vim',
