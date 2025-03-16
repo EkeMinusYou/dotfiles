788199ce224bc3a8cf05f51d9637aa41503d125d
EkeMinusYou <euonymuslke@gmail.com>
2025-03-16 16:57:53 +0900

    nvim/treesitter: resolve conflict keymap

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -33,11 +33,11 @@ return {
           swap = {
             enable = true,
             swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
               ['<leader>A'] = '@parameter.inner',
             },
+            -- swap_previous = {
+            --   ['<leader>A'] = '@parameter.inner',
+            -- },
           },
         },
 
