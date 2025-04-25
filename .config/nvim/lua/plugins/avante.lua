b5000524d4bd3d549cada8678f196a97b714e6b9
EkeMinusYou <euonymuslke@gmail.com>
2025-04-26 04:07:57 +0900

    nvim/img-clip: disable insert_mode false

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -72,7 +72,7 @@ return {
           embed_image_as_base64 = false,
           prompt_for_file_name = false,
           drag_and_drop = {
-            insert_mode = true,
+            insert_mode = false,
           },
           dir_path = 'assets',
           extension = 'png',
