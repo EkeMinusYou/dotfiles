e74b2a2c714d1d4f1e1cc0c023cf2cce970d9f9f
EkeMinusYou <euonymuslke@gmail.com>
2025-04-26 17:10:11 +0900

    rename ogpmd to unfurl

--- .config/nvim/lua/plugins/ogpmd.lua
+++
@@ -1,5 +0,0 @@
-return {
-  dir = '/Users/mineo/src/github.com/EkeMinusYou/ogpmd.nvim', -- Path to your local plugin
-  dependencies = { 'vim-denops/denops.vim' },
-  dev = true,
-}
---
+++ .config/nvim/lua/plugins/unfurl.lua
@@ -0,0 +1,5 @@
+return {
+  dir = '/Users/mineo/src/github.com/EkeMinusYou/unfurl.nvim', -- Path to your local plugin
+  dependencies = { 'vim-denops/denops.vim' },
+  dev = true,
+}
