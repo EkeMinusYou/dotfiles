8d6b473882222faf7274e8c6bfebfb287b66257a
EkeMinusYou <euonymuslke@gmail.com>
2025-04-29 17:30:15 +0900

    nvim: use remote unfurl.nvim

--- .config/nvim/lua/plugins/unfurl.lua
+++ .config/nvim/lua/plugins/unfurl.lua
@@ -1,5 +1,4 @@
 return {
-  dir = '/Users/mineo/src/github.com/EkeMinusYou/unfurl.nvim', -- Path to your local plugin
-  dependencies = { 'vim-denops/denops.vim' },
-  dev = true,
+  'EkeMinusYou/unfurl.nvim',
+  dependencies = { 'vim-denops/denops.vim', 'HakonHarnes/img-clip.nvim' },
 }
