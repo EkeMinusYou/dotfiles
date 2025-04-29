b27f117b8db6308fc35816e42a7f57a60d949618
EkeMinusYou <euonymuslke@gmail.com>
2025-04-29 17:31:50 +0900

    unfurl: comment as dev

--- .config/nvim/lua/plugins/unfurl.lua
+++ .config/nvim/lua/plugins/unfurl.lua
@@ -2,3 +2,8 @@ return {
   'EkeMinusYou/unfurl.nvim',
   dependencies = { 'vim-denops/denops.vim', 'HakonHarnes/img-clip.nvim' },
 }
+-- return {
+--   dir = '/Users/mineo/src/github.com/EkeMinusYou/unfurl.nvim', -- Path to your local plugin
+--   dependencies = { 'vim-denops/denops.vim' },
+--   dev = true,
+-- }
