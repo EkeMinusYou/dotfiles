3a9c106fb141a6e47f786389ae0cbb8c5bf1213e
EkeMinusYou <euonymuslke@gmail.com>
2025-05-13 05:46:03 +0900

    fix

--- .config/nvim/after/lsp/biome.lua
+++ .config/nvim/after/lsp/biome.lua
@@ -1,6 +1,8 @@
 ---@type vim.lsp.Config
 return {
-  root_dir = function(bufnr, callback)
-    return callback(require('lspconfig').util.root_pattern('biome.json', 'biome.toml', '.git')(bufnr))
-  end,
+  root_markers = {
+    'biome.json',
+    'biome.toml',
+  },
+  workspace_required = true,
 }
