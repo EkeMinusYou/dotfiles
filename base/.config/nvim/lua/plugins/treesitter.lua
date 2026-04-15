aad5a99d1fc04d41eb19253f088393fd9f9c3973
EkeMinusYou <euonymuslke@gmail.com>
2026-04-16 06:12:49 +0900

    chore(nvim): update plugins and treesitter configuration

--- base/.config/nvim/lua/plugins/treesitter.lua
+++ base/.config/nvim/lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
     build = ':TSUpdate',
     lazy = false,
     config = function()
-      require('nvim-treesitter.install').ensure_installed(treesitter_languages)
+      require('nvim-treesitter').install(treesitter_languages)
 
       -- See: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/atlas.lua
       vim.treesitter.language.register('hcl', atlas_filetypes)
